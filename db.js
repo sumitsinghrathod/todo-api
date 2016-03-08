58e842ac00110c0f0df8c6a73e382c7f2e4ff6ce
Sumit Kumar <sumit.kumar@vvdntech.in>
2016-03-08 21:14:32 +0530

    postgress production

--- db.js
+++ db.js
@@ -1,9 +1,17 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined , undefined , {
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+if(env === 'production'){
+	sequelize = new Sequelize(process.env.DATABASE_URL,{
+		dialect : 'postgress'
+	});
+}
+else{
+ sequelize = new Sequelize(undefined, undefined , undefined , {
 	'dialect' : 'sqlite',
 	'storage' : __dirname + '/data/Dev-todo-api.sqlite'
 });
-
+}
 var db = {};
 
 db.todo  = sequelize.import(__dirname +'/model/todo.js');
